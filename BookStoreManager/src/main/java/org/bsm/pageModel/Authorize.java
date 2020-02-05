b9d46214f4f78f1c7ff24c9e3e23db6877479b71
GZC <1739084007@qq.com>
2020-02-05 16:27:50 +0800

    测试push

--- BookStoreManager/src/main/java/org/bsm/pageModel/Authorize.java
+++ BookStoreManager/src/main/java/org/bsm/pageModel/Authorize.java
@@ -2,6 +2,14 @@ package org.bsm.pageModel;
 
 public class Authorize {
 	private String ids;
+
+	public String getIds() {
+		return ids;
+	}
+
+	public void setIds(String ids) {
+		this.ids = ids;
+	}
 	
 	
 	
