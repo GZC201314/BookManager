43551b23132d2ce79109ee092b2a1624b0fad2fb
GZC <17366192087@163.com>
2021-01-18 16:12:42 +0800

    新增数据库连接信息加密

---
+++ BookStoreManager/src/main/java/org/bsm/util/DecryptDruidSource.java
@@ -0,0 +1,16 @@
+package org.bsm.util;
+
+import com.alibaba.druid.filter.config.ConfigTools;
+import com.alibaba.druid.pool.DruidDataSource;
+
+public class DecryptDruidSource extends DruidDataSource {
+	@Override
+	public void setUsername(String username) {
+	try {
+	username = ConfigTools.decrypt(username);
+	} catch (Exception e) {
+	e.printStackTrace();
+	}
+	super.setUsername(username);
+	}
+}
