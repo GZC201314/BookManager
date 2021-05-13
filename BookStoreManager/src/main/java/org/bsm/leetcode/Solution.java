283a33282634e13eb6b0977f55d2f8f3dfc99437
GZC201314 <1739084007@qq.com>
2021-05-13 20:55:29 +0800

    LeetCode 121-121

--- BookStoreManager/src/main/java/org/bsm/leetcode/Solution.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/Solution.java
@@ -967,9 +967,9 @@ class Solution {
         }
         for (int i = 1; i < triangle.size(); i++) {
             for (int j = 0; j < triangle.get(i).size(); j++) {
-                if(j==0){
-                    triangle.get(i).set(j,triangle.get(i - 1).get(j)+triangle.get(i).get(j));
-                }else if (j == triangle.get(i - 1).size()) {
+                if (j == 0) {
+                    triangle.get(i).set(j, triangle.get(i - 1).get(j) + triangle.get(i).get(j));
+                } else if (j == triangle.get(i - 1).size()) {
                     triangle.get(i).set(j, triangle.get(i - 1).get(j - 1) + triangle.get(i).get(j));
                 } else {
                     int min = Math.min(triangle.get(i - 1).get(j - 1), triangle.get(i - 1).get(j));
@@ -986,6 +986,46 @@ class Solution {
         return min;
     }
 
+    /**
+     * 121. 买卖股票的最佳时机
+     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     * <p>
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     * <p>
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param prices 价格数组
+     * @return
+     */
+    public static int maxProfit1(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            } else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
+            }
+        }
+        return maxprofit;
+    }
+
+//    public static int maxProfit(int[] prices) {
+//        int min = Integer.MAX_VALUE;
+//        int max = prices[0];
+//        for (int i = 0; i < prices.length; i++) {
+//            if (prices[i] < min) {
+//                min = prices[i];
+//            } else if () {
+//                maxprofit = prices[i] - min;
+//            }
+//        }
+//        return maxprofit;
+//    }
 
     public static void main(String[] args) {
         TreeNode tree = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6), new TreeNode(7)));
@@ -1024,7 +1064,7 @@ class Solution {
          */
 //		merge(arr,0, arr1,1);
 //        flatten(tree);
-        System.out.println(minimumTotal(triangle));
+        System.out.println(maxProfit1(new int[]{1, 2}));
 //		partition1(three, 0);
         long end = new Date().getTime();
         System.out.println("程序运行时间: " + (end - start));
