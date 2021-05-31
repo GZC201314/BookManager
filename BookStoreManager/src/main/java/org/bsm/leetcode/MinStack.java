5fc7fcf8bd82fd541a3e72d85c418b6f8cbb9f6c
GZC201314 <1739084007@qq.com>
2021-05-31 18:46:52 +0800

    LeetCode 154 155 160 162

--- BookStoreManager/src/main/java/org/bsm/leetcode/MinStack.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/MinStack.java
@@ -4,7 +4,11 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
- * 146. LRU 缓存机制
+ * 155. 最小栈
+ *
+ * <p>设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ *
+ * <p>push(x) —— 将元素 x 推入栈中。 pop()—— 删除栈顶的元素。 top()—— 获取栈顶元素。 getMin() —— 检索栈中的最小元素。
  *
  * @author GZC-
  * @create 2021-05-26 10:48
