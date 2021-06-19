f43f81e2a9fd1351694a825d591f2d00db8bc05c
GZC201314 <1739084007@qq.com>
2021-06-19 21:01:12 +0800

    系统整合rabbitMQ，添加测试用例

---
+++ BookStoreManager/src/test/java/org/bsm/test/rabbitMQ/RabbitMQTest.java
@@ -0,0 +1,33 @@
+package org.bsm.test.rabbitMQ;
+
+import org.junit.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.HashMap;
+
+/**
+ * @author GZC-
+ * @create 2021-06-19 20:20
+ */
+public class RabbitMQTest {
+  /** redis 整合测试 */
+  @Test
+  public void redisTest() {
+    ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring-rabbit-send.xml");
+
+    HashMap<String, String> map = new HashMap<String, String>();
+    map.put("id", "1");
+    map.put("name", "pig");
+    // 根据key发送到对应的队列
+    RabbitTemplate rabbitTemplate = (RabbitTemplate) ac.getBean("rabbitTemplate");
+    ;
+    rabbitTemplate.convertAndSend("queuekey", map);
+
+    map.put("id", "2");
+    map.put("name", "cat");
+    // 根据key发送到对应的队列
+    rabbitTemplate.convertAndSend("queuekey", map);
+  }
+}
