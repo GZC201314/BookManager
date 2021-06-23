3e1724db954263ca88a24c43f1e363e3b20eeca3
GZC201314 <1739084007@qq.com>
2021-06-23 22:32:13 +0800

    LeetCode 205

--- BookStoreManager/src/main/java/org/bsm/leetcode/Solution.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/Solution.java
@@ -2983,6 +2983,40 @@ class Solution {
     return primes.size();
   }
 
+  /**
+   * 205. 同构字符串
+   *
+   * <p>给定两个字符串 s和 t，判断它们是否是同构的。
+   *
+   * <p>如果s中的字符可以按某种映射关系替换得到t，那么这两个字符串是同构的。
+   *
+   * <p>每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。
+   *
+   * <p>不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。
+   */
+  public static boolean isIsomorphic(String s, String t) {
+    int sLen = s.length();
+    int tLen = t.length();
+    if (sLen != tLen) {
+      return false;
+    }
+    Map<Character, Character> map = new HashMap<>();
+    for (int i = 0; i < sLen; i++) {
+      if (map.containsKey(s.charAt(i))) {
+        if (map.get(s.charAt(i)) != t.charAt(i)) {
+          return false;
+        }
+      } else {
+        if (map.containsValue(t.charAt(i))) {
+          return false;
+        }
+        map.put(s.charAt(i), t.charAt(i));
+      }
+    }
+
+    return true;
+  }
+
   public static void main(String[] args) {
     TreeNode tree =
         new TreeNode(
@@ -3052,7 +3086,7 @@ class Solution {
     String[] strArr =
         new String[] {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
     rotate(preorder, 3);
-    System.out.println(removeElements(ListNode1, 3));
+    System.out.println(isIsomorphic("egg", "add"));
     //    List<String> strlist = new ArrayList<>(Arrays.asList("apple", "pen"));
     long start = new Date().getTime();
     int[] intArr = new int[] {5, 25, 75};
