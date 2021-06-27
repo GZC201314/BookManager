766150ae2bd0583d06bc7d133fc4b55de0d4f38c
GZC201314 <1739084007@qq.com>
2021-06-27 19:25:45 +0800

    LeetCode 212

--- BookStoreManager/src/main/java/org/bsm/leetcode/Solution.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/Solution.java
@@ -3159,6 +3159,89 @@ class Solution {
     canFinish_result[index--] = u;
   }
 
+  /**
+   * 212. 单词搜索 II
+   *
+   * <p>给定一个m x n 二维字符网格board和一个单词（字符串）列表 words，
+   *
+   * <p>找出所有同时在二维网格和字典中出现的单词。
+   *
+   * <p>单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+   *
+   * <p>同一个单元格内的字母在一个单词中不允许被重复使用。
+   */
+  public static char[][] _board = null;
+
+  public static ArrayList<String> _result = new ArrayList<String>();
+
+  public static List<String> findWords(char[][] board, String[] words) {
+
+    // Step 1). Construct the Trie
+    TrieNode root = new TrieNode();
+    for (String word : words) {
+      TrieNode node = root;
+
+      for (Character letter : word.toCharArray()) {
+        if (node.children.containsKey(letter)) {
+          node = node.children.get(letter);
+        } else {
+          TrieNode newNode = new TrieNode();
+          node.children.put(letter, newNode);
+          node = newNode;
+        }
+      }
+      node.word = word; // store words in Trie
+    }
+
+    _board = board;
+    // Step 2). Backtracking starting for each cell in the board
+    for (int row = 0; row < board.length; ++row) {
+      for (int col = 0; col < board[row].length; ++col) {
+        if (root.children.containsKey(board[row][col])) {
+          backtracking(row, col, root);
+        }
+      }
+    }
+
+    return _result;
+  }
+
+  public static void backtracking(int row, int col, TrieNode parent) {
+    Character letter = _board[row][col];
+    TrieNode currNode = parent.children.get(letter);
+
+    // check if there is any match
+    if (currNode.word != null) {
+      _result.add(currNode.word);
+      currNode.word = null;
+    }
+
+    // mark the current letter before the EXPLORATION
+    _board[row][col] = '#';
+
+    // explore neighbor cells in around-clock directions: up, right, down, left
+    int[] rowOffset = {-1, 0, 1, 0};
+    int[] colOffset = {0, 1, 0, -1};
+    for (int i = 0; i < 4; ++i) {
+      int newRow = row + rowOffset[i];
+      int newCol = col + colOffset[i];
+      if (newRow < 0 || newRow >= _board.length || newCol < 0 || newCol >= _board[0].length) {
+        continue;
+      }
+      if (currNode.children.containsKey(_board[newRow][newCol])) {
+        backtracking(newRow, newCol, currNode);
+      }
+    }
+
+    // End of EXPLORATION, restore the original letter in the board.
+    _board[row][col] = letter;
+
+    // Optimization: incrementally remove the leaf nodes
+    if (currNode.children.isEmpty()) {
+      parent.children.remove(letter);
+    }
+  }
+
   public static void main(String[] args) {
     TreeNode tree =
         new TreeNode(
