7e02ee742a773a290a029275a8eeddb131989ddb
GZC201314 <1739084007@qq.com>
2021-07-01 15:34:20 +0800

    LeetCode 220

--- BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
@@ -202,6 +202,50 @@ class SolutionHead {
     return false;
   }
 
+  /**
+   * 220. 存在重复元素 III
+   *
+   * <p>给你一个整数数组 nums 和两个整数k 和 t 。请你判断是否存在 两个不同下标 i 和 j，使得abs(nums[i] - nums[j]) <= t ，
+   *
+   * <p>同时又满足 abs(i- j) <= k 。
+   *
+   * <p>如果存在则返回 true，不存在返回 false。
+   */
+  public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+    int n = nums.length;
+    Map<Long, Long> map = new HashMap<Long, Long>();
+    long w = (long) t + 1;
+    for (int i = 0; i < n; i++) {
+      long id;
+      if (nums[i] >= 0) {
+        id = nums[i] / w;
+      } else {
+
+        id = (nums[i] + 1) / w - 1;
+      }
+      if (map.containsKey(id)) {
+        return true;
+      }
+      if (map.containsKey(id - 1) && Math.abs(nums[i] - map.get(id - 1)) < w) {
+        return true;
+      }
+      if (map.containsKey(id + 1) && Math.abs(nums[i] - map.get(id + 1)) < w) {
+        return true;
+      }
+      map.put(id, (long) nums[i]);
+      if (i >= k) {
+        if (nums[i - k] >= 0) {
+          id = nums[i - k] / w;
+        } else {
+
+          id = (nums[i - k] + 1) / w - 1;
+        }
+        map.remove(id);
+      }
+    }
+    return false;
+  }
+
   public static void main(String[] args) {
     System.out.println(shortestPalindrome(""));
   }
