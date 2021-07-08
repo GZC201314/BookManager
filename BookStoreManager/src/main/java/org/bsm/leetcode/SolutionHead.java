46f1b9605069fb4a7ab98d959fa4329b1615b037
GZC201314 <1739084007@qq.com>
2021-07-08 22:17:18 +0800

    LeetCode 222

--- BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
@@ -1,5 +1,7 @@
 package org.bsm.leetcode;
 
+import org.bsm.leetcode.model.TreeNode;
+
 import java.util.*;
 
 /**
@@ -289,6 +291,54 @@ class SolutionHead {
       }
     }
     return max * max;
+  }
+
+  /**
+   * 222. 完全二叉树的节点个数
+   *
+   * <p>给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+   */
+  public static int countNodes(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    return 1 + countNodes(root.left) + countNodes(root.right);
+  }
+
+  public int countNodes1(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int level = 0;
+    TreeNode node = root;
+    while (node.left != null) {
+      level++;
+      node = node.left;
+    }
+    int low = 1 << level, high = (1 << (level + 1)) - 1;
+    while (low < high) {
+      int mid = (high - low + 1) / 2 + low;
+      if (exists(root, level, mid)) {
+        low = mid;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return low;
+  }
+
+  public boolean exists(TreeNode root, int level, int k) {
+    int bits = 1 << (level - 1);
+    TreeNode node = root;
+    while (node != null && bits > 0) {
+      if ((bits & k) == 0) {
+        node = node.left;
+      } else {
+        node = node.right;
+      }
+      bits >>= 1;
+    }
+    return node != null;
   }
 
   public static void main(String[] args) {
