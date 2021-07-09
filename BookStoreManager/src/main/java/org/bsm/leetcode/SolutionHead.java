14c1c6c5ef02d24f4cc1ea53d87eadff78015653
GZC201314 <1739084007@qq.com>
2021-07-09 16:08:11 +0800

    LeetCode 223-224

--- BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
@@ -357,6 +357,11 @@ class SolutionHead {
     return mainArea - subArea;
   }
 
+  /**
+   * 224. 基本计算器
+   *
+   * <p>给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+   */
   public static int calculate(String s) {
     s = s.replaceAll(" ", "");
     Deque<Integer> ops = new ArrayDeque<>();
