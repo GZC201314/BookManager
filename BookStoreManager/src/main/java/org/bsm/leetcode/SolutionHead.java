61728a4d5e8b925e66ce07045418bcb090a28ae8
GZC201314 <1739084007@qq.com>
2021-07-16 23:27:17 +0800

    LeetCode 239

--- BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
@@ -750,6 +750,81 @@ class SolutionHead {
     return result;
   }
 
+  /**
+   * 238. 滑动窗口最大值
+   *
+   * <p>给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位
+   *
+   * @param nums
+   * @param k
+   * @return
+   */
+  public static int[] maxSlidingWindow(int[] nums, int k) {
+    //    int start = 0, end = k;
+    //    int length = nums.length;
+    //    int[] result = new int[length - k + 1];
+    //    int max = nums[0];
+    //    for (int i = start; i < end; i++) {
+    //      if (nums[i] > max) {
+    //        max = nums[i];
+    //      }
+    //    }
+    //    int index = 0;
+    //    result[index++] = max;
+    //    while (end < length) {
+    //      // 如果删除的数字不是max
+    //      if (nums[start] != max) {
+    //        // 判断新加入的数字是否比max大
+    //        if (max < nums[end]) {
+    //          result[index++] = nums[end];
+    //          max = nums[end];
+    //        } else {
+    //          result[index++] = max;
+    //        }
+    //      } else { // 删除的是最大值
+    //        // 新加入的数字比max大
+    //        if (max < nums[end]) {
+    //          result[index++] = nums[end];
+    //          max = nums[end];
+    //        } else { // 新加入的数字比max小。找出窗口中的最大值
+    //          max = nums[start + 1];
+    //          for (int i = start + 1; i <= end; i++) {
+    //            if (nums[i] > max) {
+    //              max = nums[i];
+    //            }
+    //          }
+    //          result[index++] = max;
+    //        }
+    //      }
+    //      start++;
+    //      end++;
+    //    }
+    //    return result;
+    if (nums == null || nums.length < 2) return nums;
+    // 双向队列 保存当前窗口最大值的数组位置 保证队列中数组位置的数值按从大到小排序
+    LinkedList<Integer> queue = new LinkedList<Integer>();
+    // 结果数组
+    int[] result = new int[nums.length - k + 1];
+    // 遍历nums数组
+    for (int i = 0; i < nums.length; i++) {
+      // 保证从大到小 如果前面数小则需要依次弹出，直至满足要求
+      while (!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]) {
+        queue.pollLast();
+      }
+      // 添加当前值对应的数组下标
+      queue.addLast(i);
+      // 判断当前队列中队首的值是否有效
+      if (queue.peek() <= i - k) {
+        queue.poll();
+      }
+      // 当窗口长度为k时 保存当前窗口中最大值
+      if (i + 1 >= k) {
+        result[i + 1 - k] = nums[queue.peek()];
+      }
+    }
+    return result;
+  }
+
   public static void main(String[] args) {
     char[][] matrix =
         new char[][] {
@@ -759,7 +834,7 @@ class SolutionHead {
           {'1', '1', '1', '1', '1'},
           {'0', '0', '1', '1', '1'}
         };
-    int[] arr = {1, 2, 3, 4};
-    System.out.println(Arrays.toString(productExceptSelf(arr)));
+    int[] arr = {7, 2, 4};
+    System.out.println(Arrays.toString(maxSlidingWindow(arr, 2)));
   }
 }
