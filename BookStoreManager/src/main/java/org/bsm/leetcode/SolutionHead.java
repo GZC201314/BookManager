a03a7905be539ceaa5487098be8d55aa710188f4
GZC201314 <1739084007@qq.com>
2021-08-03 19:29:01 +0800

    LeetCode 268 273

--- BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
@@ -1045,6 +1045,165 @@ class SolutionHead {
     return dp[n];
   }
 
+  /**
+   * 268. 丢失的数字
+   *
+   * <p>给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+   *
+   * <p>排序算法
+   */
+  public static int missingNumber1(int[] nums) {
+    Arrays.sort(nums);
+    if (nums[0] != 0) {
+      return 0;
+    }
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i - 1] - nums[i] != -1) {
+        return nums[i - 1] + 1;
+      }
+    }
+    return nums[nums.length - 1] + 1;
+  }
+  /**
+   * 268. 丢失的数字
+   *
+   * <p>给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+   *
+   * <p>位运算算法
+   */
+  public static int missingNumber(int[] nums) {
+    int result = nums.length;
+    for (int i = 0; i < nums.length; i++) {
+      result ^= i ^ nums[i];
+    }
+    return result;
+  }
+
+  /** 个位整数 数字到文字的转换 */
+  public String one(int num) {
+    switch (num) {
+      case 1:
+        return "One";
+      case 2:
+        return "Two";
+      case 3:
+        return "Three";
+      case 4:
+        return "Four";
+      case 5:
+        return "Five";
+      case 6:
+        return "Six";
+      case 7:
+        return "Seven";
+      case 8:
+        return "Eight";
+      case 9:
+        return "Nine";
+    }
+    return "";
+  }
+
+  /** 小于20的整数的数字到文字的转换 */
+  public String twoLessThan20(int num) {
+    switch (num) {
+      case 10:
+        return "Ten";
+      case 11:
+        return "Eleven";
+      case 12:
+        return "Twelve";
+      case 13:
+        return "Thirteen";
+      case 14:
+        return "Fourteen";
+      case 15:
+        return "Fifteen";
+      case 16:
+        return "Sixteen";
+      case 17:
+        return "Seventeen";
+      case 18:
+        return "Eighteen";
+      case 19:
+        return "Nineteen";
+    }
+    return "";
+  }
+
+  /** 整十位的数字到文字的转换 */
+  public String ten(int num) {
+    switch (num) {
+      case 2:
+        return "Twenty";
+      case 3:
+        return "Thirty";
+      case 4:
+        return "Forty";
+      case 5:
+        return "Fifty";
+      case 6:
+        return "Sixty";
+      case 7:
+        return "Seventy";
+      case 8:
+        return "Eighty";
+      case 9:
+        return "Ninety";
+    }
+    return "";
+  }
+
+  /** 两位数的数字到文字的转换 */
+  public String two(int num) {
+    if (num == 0) return "";
+    else if (num < 10) return one(num);
+    else if (num < 20) return twoLessThan20(num);
+    else {
+      int tenner = num / 10;
+      int rest = num - tenner * 10;
+      if (rest != 0) return ten(tenner) + " " + one(rest);
+      else return ten(tenner);
+    }
+  }
+
+  /** 三位数的数字到文字的转换 */
+  public String three(int num) {
+    int hundred = num / 100;
+    int rest = num - hundred * 100;
+    String res = "";
+    if (hundred * rest != 0) res = one(hundred) + " Hundred " + two(rest);
+    else if ((hundred == 0) && (rest != 0)) res = two(rest);
+    else if ((hundred != 0) && (rest == 0)) res = one(hundred) + " Hundred";
+    return res;
+  }
+
+  /** 273. 整数转换英文表示 */
+  public String numberToWords(int num) {
+    if (num == 0) return "Zero";
+
+    int billion = num / 1000000000;
+    int million = (num - billion * 1000000000) / 1000000;
+    int thousand = (num - billion * 1000000000 - million * 1000000) / 1000;
+    int rest = num - billion * 1000000000 - million * 1000000 - thousand * 1000;
+
+    String result = "";
+    if (billion != 0) result = three(billion) + " Billion";
+    if (million != 0) {
+      if (!result.isEmpty()) result += " ";
+      result += three(million) + " Million";
+    }
+    if (thousand != 0) {
+      if (!result.isEmpty()) result += " ";
+      result += three(thousand) + " Thousand";
+    }
+    if (rest != 0) {
+      if (!result.isEmpty()) result += " ";
+      result += three(rest);
+    }
+    return result;
+  }
+
   public static void main(String[] args) {
     char[][] matrix =
         new char[][] {
