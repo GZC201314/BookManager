879af6c2b90034d70085e8b0d52307a9c7d0b8ac
GZC201314 <1739084007@qq.com>
2021-08-24 22:38:36 +0800

    LeetCode 309

--- BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
@@ -1693,6 +1693,24 @@ class SolutionHead {
     return false;
   }
 
+  public int maxProfit(int[] prices) {
+    if (prices.length == 0) {
+      return 0;
+    }
+    int length = prices.length;
+    int[][] dp = new int[length][3];
+    // dp[i][0] 持有股票的最大收益
+    // dp[i][1] 不持有股票，且处于冷冻期的最大收益
+    // dp[i][2] 不持有股票，且不处于冷冻期的最大收益
+    dp[0][0] = -prices[0];
+    for (int i = 1; i < length; i++) {
+      dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+      dp[i][1] = dp[i - 1][0] + prices[i];
+      dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1]);
+    }
+    return Math.max(dp[length - 1][1], dp[length - 1][2]);
+  }
+
   public static void main(String[] args) {
     char[][] matrix =
         new char[][] {
