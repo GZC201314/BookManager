8299a880477236f9a211c8c57d8b51d655f0db58
GZC201314 <1739084007@qq.com>
2021-09-02 22:57:20 +0800

    LeetCode 315

--- BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
+++ BookStoreManager/src/main/java/org/bsm/leetcode/SolutionHead.java
@@ -1784,6 +1784,78 @@ class SolutionHead {
       s[end--] = tem;
     }
   }
+  /*
+   *315. 计算右侧小于当前元素的个数
+   *
+   * 归并排序加逆序数
+   * */
+  public List<Integer> countSmaller(int[] nums) {
+    List<Integer> result = new ArrayList<>();
+    int len = nums.length;
+    if (len == 0) {
+      return result;
+    }
+
+    int[] temp = new int[len];
+    int[] res = new int[len];
+
+    // 索引数组，作用：归并回去的时候，方便知道是哪个下标的元素
+    int[] indexes = new int[len];
+    for (int i = 0; i < len; i++) {
+      indexes[i] = i;
+    }
+    mergeAndCountSmaller(nums, 0, len - 1, indexes, temp, res);
+
+    // 把 int[] 转换成为 List<Integer>，没有业务逻辑
+    for (int i = 0; i < len; i++) {
+      result.add(res[i]);
+    }
+    return result;
+  }
+
+  /** 针对数组 nums 指定的区间 [left, right] 进行归并排序，在排序的过程中完成统计任务 */
+  private void mergeAndCountSmaller(
+      int[] nums, int left, int right, int[] indexes, int[] temp, int[] res) {
+    if (left == right) {
+      return;
+    }
+    int mid = left + (right - left) / 2;
+    mergeAndCountSmaller(nums, left, mid, indexes, temp, res);
+    mergeAndCountSmaller(nums, mid + 1, right, indexes, temp, res);
+
+    // 归并排序的优化，如果索引数组有序，则不存在逆序关系，没有必要合并
+    if (nums[indexes[mid]] <= nums[indexes[mid + 1]]) {
+      return;
+    }
+    mergeOfTwoSortedArrAndCountSmaller(nums, left, mid, right, indexes, temp, res);
+  }
+
+  /** [left, mid] 是排好序的，[mid + 1, right] 是排好序的 */
+  private void mergeOfTwoSortedArrAndCountSmaller(
+      int[] nums, int left, int mid, int right, int[] indexes, int[] temp, int[] res) {
+    if (right + 1 - left >= 0) System.arraycopy(indexes, left, temp, left, right + 1 - left);
+
+    int i = left;
+    int j = mid + 1;
+    for (int k = left; k <= right; k++) {
+      if (i > mid) {
+        indexes[k] = temp[j];
+        j++;
+      } else if (j > right) {
+        indexes[k] = temp[i];
+        i++;
+        res[indexes[k]] += (right - mid);
+      } else if (nums[temp[i]] <= nums[temp[j]]) {
+        // 注意：这里是 <= ，保证稳定性
+        indexes[k] = temp[i];
+        i++;
+        res[indexes[k]] += (j - mid - 1);
+      } else {
+        indexes[k] = temp[j];
+        j++;
+      }
+    }
+  }
 
   public static void main(String[] args) {
     char[][] matrix =
